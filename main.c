798ccb7e2527620135fbc3d6c6dfc6495107af28
Azmain Inquaid Haque <azmaininquaidhaque@gmail.com>
2024-02-03 17:33:47 +0600

    cleaned

--- main.c
+++ main.c
@@ -1,31 +1,9 @@
 
 #include "InterFace.h"
 
-typedef struct {
-    char name[buffer_size];
-    char password[3222];
-    float amount;
-
-} acnt;
-
 
 int main() {
-//    acnt ac;
-//    char store[2255] = "AXM";
-//    strcpy(ac.password, store);
-//
-////    printf("%s", ac.password);
-////char *temp = SHA256(ac.password);
-//    strcpy(ac.password, SHA256(ac.password));
-//
-//    printf("%s", ac.password);
 
-//char data[] = "Tonti";
-//printf("%s",output);
-//    Display();
-//char name[] = "AZmain";
-//char *output = SHA256(name);
-//    printf("%s",output);
     Display();
     return 0;
 }
